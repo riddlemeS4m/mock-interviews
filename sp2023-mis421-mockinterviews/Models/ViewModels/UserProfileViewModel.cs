aca04a477a5b9cea6b7b337e2afbf30de5366919
Sam Riddle <swriddle@crimson.ua.edu>
2026-08-24 22:56:28 -0500

    Remove Google Drive functionality from model layer

--- sp2023-mis421-mockinterviews/Models/ViewModels/UserProfileViewModel.cs
+++ sp2023-mis421-mockinterviews/Models/ViewModels/UserProfileViewModel.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using sp2023_mis421_mockinterviews.Data.Constants;
 using sp2023_mis421_mockinterviews.Models.UserDb;
-using sp2023_mis421_mockinterviews.Services.GoogleDrive;
 using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
 
 namespace sp2023_mis421_mockinterviews.Areas.Identity.Pages.Account.Manage
 {
@@ -13,29 +11,19 @@ namespace sp2023_mis421_mockinterviews.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly GoogleDriveResumeService _driveResumeService;
-        private readonly GoogleDrivePfpService _drivePfpService;
 
         public UserProfileViewModel(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            GoogleDriveResumeService driveResumeService,
-            GoogleDrivePfpService drivePfpService)
+            SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _driveResumeService = driveResumeService;
-            _drivePfpService = drivePfpService;
         }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public string Username { get; set; }
-        public string Id { get; set; }
-
-
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -72,36 +60,21 @@ namespace sp2023_mis421_mockinterviews.Areas.Identity.Pages.Account.Manage
             public Classes Class { get; set; }
             [Display(Name = "Company")]
             public string? Company { get; set; }
-            // [Display(Name = "Profile Picture")]
-            // public byte[]? ProfilePicture { get; set; }
-            // [Display(Name = "Resume")]
-            // public byte[]? Resume { get; set; }
-            [Display(Name = "Profile Picture")]
-            public string? ProfilePicture { get; set; }
-            [Display(Name = "Resume")]
-            public string? Resume { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
-            var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             var firstName = user.FirstName;
             var lastName = user.LastName;
             var userClass = user.Class;
-            var profilePicture = user.ProfilePicture;
-            var resume = user.Resume;
             var company = user.Company;
-            Username = userName;
-            Id = user.Id;
             Input = new InputModel
             {
                 PhoneNumber = phoneNumber,
                 FirstName = firstName,
                 LastName = lastName,
                 Class = userClass,
-                ProfilePicture = profilePicture,
-                Resume = resume,
                 Company = company
             };
         }
@@ -161,56 +134,6 @@ namespace sp2023_mis421_mockinterviews.Areas.Identity.Pages.Account.Manage
             {
                 user.Class = Input.Class;
                 await _userManager.UpdateAsync(user);
-            }
-
-            if (Request.Form.Files.Count > 0)
-            {
-                // profile picture
-                var profilePictureFile = Request.Form.Files["profilePicture"];
-                if (profilePictureFile != null)
-                {
-                    if(user.ProfilePicture != null)
-                    {
-                        try
-                        {
-                            await _drivePfpService.DeleteFile(user.ProfilePicture);
-                        }
-                        catch
-                        {
-                            user.ProfilePicture = null;
-                        }
-                    }
-
-                    var fileId = await _drivePfpService.UploadFile(profilePictureFile);
-                    user.ProfilePicture = fileId;
-
-                    await _userManager.UpdateAsync(user);
-                }
-
-                // resume
-                if (User.IsInRole(RolesConstants.StudentRole))
-                {
-                    var resumeFile = Request.Form.Files["resume"];
-                    if (resumeFile != null)
-                    {
-                        if (user.Resume != null)
-                        {
-                            try
-                            {
-                                await _driveResumeService.DeleteFile(user.Resume);
-                            }
-                            catch 
-                            { 
-                                user.Resume = null; 
-                            }
-                        }
-
-                        var fileId = await _driveResumeService.UploadFile(resumeFile);
-                        user.Resume = fileId;
-
-                        await _userManager.UpdateAsync(user);
-                    }
-                }
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
