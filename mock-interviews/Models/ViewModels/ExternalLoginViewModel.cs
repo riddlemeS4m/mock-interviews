66f54d870aaa7f1c62a4889b32de4bdd9db7343d
Sam Riddle <swriddle@crimson.ua.edu>
2026-08-27 23:19:04 -0500

    Remove now-unused view models

--- mock-interviews/Models/ViewModels/ExternalLoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MockInterviews.Models.ViewModels
-{
-    public class ExternalLoginViewModel
-    {
-        public string ReturnUrl { get; set; } = string.Empty;
-        public string Provider { get; set; } = string.Empty;
-    }
-}
